330e28453627727e35de74521104e20ec029bbe0
ARyback <adam.ryback@gmail.com>
2022-05-09 20:31:10 -0500

    Imported the different components to App.jsx and returned them in the App function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import SearchBar from "./components/SearchBar/SearchBar";
+import MusicTable from "./components/MusicTable/MusicTable";
 
 function App() {
-  return <div>hello world</div>;
+  return <div>
+  <NavigationBar />
+  <SearchBar />
+  <MusicTable />
+  </div>;
 }
 
 export default App;
