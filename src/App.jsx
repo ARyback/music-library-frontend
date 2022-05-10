74da5635ce438cf8b481297cb1ec20d550f916e3
ARyback <adam.ryback@gmail.com>
2022-05-10 12:35:03 -0500

    Started a useEffect and imported Axios into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import SearchBar from "./components/SearchBar/SearchBar";
 import MusicTable from "./components/MusicTable/MusicTable";
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 function App() {
-  return <div>
-  <NavigationBar />
-  <SearchBar />
-  <MusicTable />
-  </div>;
+  
+  useEffect(() => {
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+  }, []);
+
+  return 
+    <div>
+  
+
+
+    </div>;
 }
 
 export default App;
