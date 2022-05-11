0b576f3a1d5ef95a01d26c9c765571f294112df5
ARyback <adam.ryback@gmail.com>
2022-05-11 11:46:55 -0500

    Added some initial code to AddASong component mimicking the weight-tracker app

--- src/components/AddASong/AddASong.jsx
+++ src/components/AddASong/AddASong.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const AddASong = (props) => {
-    return <div>This is an add a song bar</div>;
+    
+    const [title, setTitle] = useState("");
+    const [artist, setArtist] = useState("");
+    const [album, setAlbum] = useState("");
+    const [genre, setGenre] = useState("");
+    const [releaseDate, setReleaseDate] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newSong = {
+            title: title,
+            artist: artist,
+            album: album,
+            genre: genre,
+            releaseDate: releaseDate,
+        };
+        props.addNewSongProperty(newSong);
+    }
+
+    return (
+        <div>test</div>
+        );
 }
  
 export default AddASong;
